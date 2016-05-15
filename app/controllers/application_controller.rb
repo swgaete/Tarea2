afdb7a9d20295c2f08c4a2449d076cce2c61579c
swgaete <swgaete@uc.cl>
2016-05-15 02:14:24 -0400

    app 1.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,25 +2,6 @@ require 'net/http'
 require 'json'
 
 class ApplicationController < ActionController::Base
-def crearDatos(cantidad, media)
-    respuesta={ "metadata" => {},
-                "posts" => [],
-                "version" => ""
-    }
-    respuesta['metadata']=cantidad['data']['media_count']
-
-    media['data'].each do |item|
-    respuesta['posts']<<{
-     'tags'=> item['tags']
-     'username'=> item['user']['username']
-     'likes'=> item['likes']
-     'url'=> maximaResolucion(item['images'])
-     'caption' =>  item['caption']['text']
-    }
-    #falta ver lo de la version y lo de la calidad de las imagenes
-    #:Bad_request para lo del error de 400
-    return reponse
-  end
 
   def parametros
 
@@ -30,9 +11,8 @@ def crearDatos(cantidad, media)
     uri= 'https://api.instagram.com/v1/tags/'+ tag.to_s + '/media/recent?access_token='+ access_token.to_s
     cantidad = get(uriCantidad)
     media = get(uri)
-    reponse = crearDatos(cantidad, media)
-    render {}
-    render json: response, root: false
+    respuesta = crearDatos(cantidad, media)
+    render json: respuesta, root: false
 
   end
 
@@ -42,12 +22,33 @@ def crearDatos(cantidad, media)
     http.use_ssl=true
     request = Net::HTTP::Get.new(uri.request_uri)
     respuesta = http.request(request)
-    return JSON.parse(respuest.body)
+    return JSON.parse(respuesta.body)
+    rescue JSON::ParserError
+    return {}
+  end
+  
+  def crearDatos(cantidad, media)
+    respuesta={ "metadata" => {},
+                "posts" => [],
+                "version" => ""
+    }
+    respuesta['metadata']={'total' => cantidad['data']['media_count']}
+    respuesta['version'] = '1.1'
+    media['data'].each do |item| 
+    respuesta['posts']<<{
+     'tags'=> item['tags'],
+     'username' => item['user']['username'],
+     'likes'=> item['likes']['count'],
+     'url'=> item['images']['standard_resolution']['url'], #siempre hay standard resolution
+     'caption' =>  item['caption']['text']
+    }
+    #:Bad_request para lo del error de 400
+    return respuesta
+    
   end
 
- 
-
   def maximaResolucion(imagen)
+  	
     if imagen.has_key?('standard_resolution')
       return imagen['standard_resolution']['url']
     elsif imagen.has_key?('low_resolution')
@@ -55,11 +56,12 @@ def crearDatos(cantidad, media)
     else
       return imagen['thumbnail']['url']
     end
+    return {}
   end
-  end    
+      
+end
   #protect_from_forgery with: :exception
-<<<<<<< HEAD
+
+
 end
-=======
-end
->>>>>>> master
+
